d745d4df6cddc6c00e57ebcaaa7d9fab82d2abf4
FAL-coffee <fal.engineer.2001@gmail.com>
2023-04-17 00:30:19 +0900

    :wrench: appConfigにdb設定を追加

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,3 +1,10 @@
 export const appConfig = () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
+  database: {
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.DATABASE_USERNAME || 'rootuser',
+    password: process.env.DATABASE_PASSWORD || 'passw0rd',
+    database: process.env.DATABASE_NAME || 'todoapp',
+  },
 });
